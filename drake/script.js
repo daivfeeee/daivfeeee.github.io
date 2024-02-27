b7f11b4927e9e9c12ed9d15703ef11c8909bedc6
daivfeeee <152994720+daivfeeee@users.noreply.github.com>
2024-02-27 07:50:14 -0700

    Add files via upload

---
+++ drake/script.js
@@ -0,0 +1,21 @@
+console.log("Hi how are you")
+const(Click)
+
+function changeSize() {
+    document.getElementById("smile"), 
+}
+
+function changeColor(elementID){
+const smile = document.getElementById("smile")
+smile.style.color = "rgb("
+}
+if("smile") === 
+
+if (d === “smile”) {
+    if (element.style.color === "purple") {
+      element.style.color = 'red';
+    } 
+else if (element.style.color === blue “” {
+      element.style.color = “yellow”;
+
+eer
